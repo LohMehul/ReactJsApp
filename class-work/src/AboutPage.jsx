4e01bf8ad6e93f4b2db89eb7169d9c3a8ce73a93
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-03-23 10:19:37 +0530

    class and function

---
+++ class-work/src/AboutPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const AboutPage = () => {
+    // return (
+    //         "AboutPage"
+    // );
+    // return (
+    //     <div className="div">
+    //         <p>something</p>
+    //         <p>Test</p>
+    //     </div>
+    // );
+    return (
+        <>
+            <p>something</p>
+            <p>Test</p>
+        </>
+    );
+};
+
+export default AboutPage;
