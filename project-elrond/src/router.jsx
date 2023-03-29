b0fbf0db4b5d612a48fb5a4a8225b580983f2398
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-03-29 10:45:26 +0530

    comments

--- project-elrond/src/router.jsx
+++ project-elrond/src/router.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import {createBrowserRouter} from "react-router-dom";
-import HomePage from "./HomePage.jsx";
-import AboutPage from "./AboutPage.jsx";
+import HomePage from "./HomePage.jsx"; //import home page as HomePage , but before import it must be exported .
+import AboutPage from "./AboutPage.jsx"; //import AboutPage as AboutPage , but before import it must be exported .
 // import ContactPage from "./ContactPage.jsx";
-import HeaderComp from "./CommonCompo/Header.jsx";
-import ExampleCompo from "./ExampleCompo.jsx";
+import HeaderComp from "./CommonCompo/Header.jsx"; //import header page as HeaderComp , but before import it must be exported .
+import ExampleCompo from "./ExampleCompo.jsx"; //import ExampleCompo page as ExampleCompo , but before import it must be exported .
 
 const MainRouter = createBrowserRouter([
     {
-        path: "/",
+        path: "/", // this path show Home Page or main index page
         element: <><HeaderComp /><HomePage /></>,
     },
     {
-        path: "/about",
+        path: "/about", // if URL give /about after home page or index page below mentioned Components. 
         element: <><HeaderComp /><AboutPage /></>,
     },
     {
         path: "/examples",
-        element: <><HeaderComp /><ExampleCompo /></>,
+        element: <><HeaderComp /><ExampleCompo /></>, // if URL give /about after home page or index page below mentioned Components
     },
 ]);
 
-export default MainRouter;
+export default MainRouter;  // export router to access it whenever it was imported , to access router out side to this file you must(compulsory) have to export it.
