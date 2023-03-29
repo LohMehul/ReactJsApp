f87f586c381c83032df758bb1a0845fe501490ea
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-03-29 12:07:48 +0530

    update

--- project-elrond/src/ClassCompo/ClassCompoRoute.jsx
+++ project-elrond/src/ClassCompo/ClassCompoRoute.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ClassCompoMenu from './ClassCompoMenu';
-import WelcomeToClassCompo from './01WelcomeToClassCompo.jsx';
-import ClassCompoConstructor from './02ClassCompoConstructor.jsx';
-import ClassCompoState from './03ClassCompoState.jsx';
-import ClassCompojsx from './04ClassCompojsx.jsx';
+// import WelcomeToClassCompo from './01WelcomeToClassCompo.jsx';
+// import ClassCompoConstructor from './02ClassCompoConstructor.jsx';
+// import ClassCompoState from './03ClassCompoState.jsx';
+// import ClassCompojsx from './04ClassCompojsx.jsx';
 import ProspExample from './05Props';
-import StateLifeCycle from './07statelifecycle';
-import ClassCompoLoader from './08statelifecycleloader';
+// import StateLifeCycle from './07statelifecycle';
+// import ClassCompoLoader from './08statelifecycleloader';
 
 class ClassCompoRoute extends Component {
     render() {
@@ -15,13 +15,13 @@ class ClassCompoRoute extends Component {
             <>
                 <Routes>
                     <Route path="/" element={<ClassCompoMenu />} >
-                        <Route path="classcompointro" element={<WelcomeToClassCompo />} />
-                        <Route path="classcompoconstructor" element={<ClassCompoConstructor />} />
-                        <Route path="classcompostate" element={<ClassCompoState />} />
-                        <Route path="classcompojsx" element={<ClassCompojsx />} />
+                        {/* <Route path="classcompointro" element={<WelcomeToClassCompo />} /> */}
+                        {/* <Route path="classcompoconstructor" element={<ClassCompoConstructor />} /> */}
+                        {/* <Route path="classcompostate" element={<ClassCompoState />} /> */}
+                        {/* <Route path="classcompojsx" element={<ClassCompojsx />} /> */}
                         <Route path="classcompoporps" element={<ProspExample />} />
-                        <Route path="classcompostatelifecycle" element={<StateLifeCycle />} />
-                        <Route path="classcompostatelifecycleloader" element={<ClassCompoLoader />} />
+                        {/* <Route path="classcompostatelifecycle" element={<StateLifeCycle />} /> */}
+                        {/* <Route path="classcompostatelifecycleloader" element={<ClassCompoLoader />} /> */}
                     </Route>
                 </Routes>
             </>
