126fed825581d8e03a26d445cd44ef45cb0ccc75
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-03-30 18:27:23 +0530

    update

--- project-elrond/src/ExampleCompo.jsx
+++ project-elrond/src/ExampleCompo.jsx
@@ -1,66 +1,76 @@
 import React from 'react';
-import {
-    MDBCard,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBCardText,
-    MDBCardImage,
-    MDBBtn
-} from 'mdb-react-ui-kit';    
+import ClassAndFunctionCompo from "./ClassAndFunctionCompo.jsx"
+
+
 
 const ExampleCompo = () => {
     return (
-
-        // card code form bootstrap 
-        <>
-            <div className='container'>
-                <div className='col'>
-                    <div className='row'>
-                        <MDBCard className='col-3'>
-                            <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
-                            <MDBCardBody>
-                                <MDBCardTitle>Card title</MDBCardTitle>
-                                <MDBCardText>
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                                </MDBCardText>
-                                <MDBBtn href='#'>Button</MDBBtn>
-                            </MDBCardBody>
-                        </MDBCard>
-                        <MDBCard className='col-3'>
-                            <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
-                            <MDBCardBody>
-                                <MDBCardTitle>Card title</MDBCardTitle>
-                                <MDBCardText>
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                                </MDBCardText>
-                                <MDBBtn href='#'>Button</MDBBtn>
-                            </MDBCardBody>
-                        </MDBCard>
-                        <MDBCard className='col-3'>
-                            <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
-                            <MDBCardBody>
-                                <MDBCardTitle>Card title</MDBCardTitle>
-                                <MDBCardText>
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                                </MDBCardText>
-                                <MDBBtn href='#'>Button</MDBBtn>
-                            </MDBCardBody>
-                        </MDBCard>
-                        <MDBCard className='col-3'>
-                            <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
-                            <MDBCardBody>
-                                <MDBCardTitle>Card title</MDBCardTitle>
-                                <MDBCardText>
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                                </MDBCardText>
-                                <MDBBtn href='#'>Button</MDBBtn>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </div>
-                </div>
+        <div className='container'>
+        <div className="row mt-3">
+            <div className="col">
+                <h2 className='text-center'>Example Component</h2>
             </div>
-        </>
+        </div>
+        <ClassAndFunctionCompo/>
+        </div>
     );
 };
 
-export default ExampleCompo;  //component exported
+export default ExampleCompo;
+
+// const ExampleCompo = () => {
+//     return (
+
+//         // card code form bootstrap 
+//         <>
+//             <div className='container'>
+//                 <div className='col'>
+//                     <div className='row'>
+//                         <MDBCard className='col-3'>
+//                             <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+//                             <MDBCardBody>
+//                                 <MDBCardTitle>Card title</MDBCardTitle>
+//                                 <MDBCardText>
+//                                     Some quick example text to build on the card title and make up the bulk of the card's content.
+//                                 </MDBCardText>
+//                                 <MDBBtn href='#'>Button</MDBBtn>
+//                             </MDBCardBody>
+//                         </MDBCard>
+//                         <MDBCard className='col-3'>
+//                             <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+//                             <MDBCardBody>
+//                                 <MDBCardTitle>Card title</MDBCardTitle>
+//                                 <MDBCardText>
+//                                     Some quick example text to build on the card title and make up the bulk of the card's content.
+//                                 </MDBCardText>
+//                                 <MDBBtn href='#'>Button</MDBBtn>
+//                             </MDBCardBody>
+//                         </MDBCard>
+//                         <MDBCard className='col-3'>
+//                             <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+//                             <MDBCardBody>
+//                                 <MDBCardTitle>Card title</MDBCardTitle>
+//                                 <MDBCardText>
+//                                     Some quick example text to build on the card title and make up the bulk of the card's content.
+//                                 </MDBCardText>
+//                                 <MDBBtn href='#'>Button</MDBBtn>
+//                             </MDBCardBody>
+//                         </MDBCard>
+//                         <MDBCard className='col-3'>
+//                             <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/184.webp' position='top' alt='...' />
+//                             <MDBCardBody>
+//                                 <MDBCardTitle>Card title</MDBCardTitle>
+//                                 <MDBCardText>
+//                                     Some quick example text to build on the card title and make up the bulk of the card's content.
+//                                 </MDBCardText>
+//                                 <MDBBtn href='#'>Button</MDBBtn>
+//                             </MDBCardBody>
+//                         </MDBCard>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default ExampleCompo;  //component exported
