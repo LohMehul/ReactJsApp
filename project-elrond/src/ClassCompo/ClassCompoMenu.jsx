ea930740b7cbfb814ca9dec6fd299e8b11603d04
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-03-30 18:03:51 +0530

    class compo menu

---
+++ project-elrond/src/ClassCompo/ClassCompoMenu.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link,Outlet } from 'react-router-dom';
+
+class ClassCompoIntro extends Component {
+  render() {
+    return (
+      <>
+        <ul>
+          <li><Link to="classcompointro">Class Compo Intro</Link></li>
+          <li><Link to="classcompoconstructor">Constuctor</Link></li>
+          <li><Link to="classcompostate">State In Class Compo</Link></li>
+          <li><Link to="classcompojsx">JSX In Class Compo</Link></li>
+          <li><Link to="classcompoporps">Class Compo Props</Link></li>
+          <li><Link to="classcompostatelifecycle">Class Compo State LifeCycle</Link></li>
+          <li><Link to="classcompostatelifecycleloader">Class Compo State LifeCycle Loader</Link></li>
+        </ul>
+        <Outlet></Outlet>
+      </>
+    );
+  }
+}
+
+export default ClassCompoIntro;
--- project-elrond/src/router.jsx
+++ project-elrond/src/router.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React ,{Suspense} from "react";
 import {createBrowserRouter} from "react-router-dom";
 import HomePage from "./HomePage.jsx"; //import home page as HomePage , but before import it must be exported .
 import AboutPage from "./AboutPage.jsx"; //import AboutPage as AboutPage , but before import it must be exported .
 // import ContactPage from "./ContactPage.jsx";
 import HeaderComp from "./CommonCompo/Header.jsx"; //import header page as HeaderComp , but before import it must be exported .
 import ExampleCompo from "./ExampleCompo.jsx"; //import ExampleCompo page as ExampleCompo , but before import it must be exported .
-import ImageCompo from "./ClassCompo/05Props"
+import ImageCompo from "./ClassCompo/05Props";
+
+
+
+
+const ClassCompoRoute = React.lazy(()=>{ return import('./ClassCompo/ClassCompoRoute.jsx') })
+
 const MainRouter = createBrowserRouter([
     {
         path: "/", // this path show Home Page or main index page
@@ -17,11 +23,17 @@ const MainRouter = createBrowserRouter([
     },
     {
         path: "/examples",
-        element: <><HeaderComp /><ExampleCompo /></>, // if URL give /about after home page or index page below mentioned Components
+        element: <><HeaderComp/><ExampleCompo/></>,
+        children: [
+            {
+              path: "classcompo/*",
+              element: <Suspense fallback={<h2>Loading...</h2>}><ClassCompoRoute/></Suspense>,
+            },
+        ]
     },
     {
         path: "/images",
-        element: <><HeaderComp /><ImageCompo /></>, // if URL give /about after home page or index page below mentioned Components
+        element: <><HeaderComp /><ImageCompo /></>, // if URL give /images after home page or index page below mentioned Components
     },
 ]);
 
