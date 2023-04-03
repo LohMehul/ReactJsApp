5b3ca06ccaa08a465e525ccc642791879ae7752a
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-04-03 12:15:00 +0530

    major update

--- project-elrond/src/ClassCompo/07ListKeysArraymap.jsx
+++ project-elrond/src/ClassCompo/07ListKeysArraymap.jsx
@@ -3,29 +3,17 @@ import { Link } from 'react-router-dom';
 
 class ListKeysArraymap extends Component {
     render() {
-        // let arr = [12, 45, 5, 8, 4, 2]
-        // let MenuData =[{"home":"Home"}]
-        let MenuData ={"/":"Home","/about":"About"}
-        // let printData = arr.forEach(element => {
-        //     // console.log(element);
-        //     return <li>Test</li>
-        // });
-        // let printData =  arr.map(()=>{
-        //     return <li>Test</li>
-        // })
-        let MenuItem =  Object.entries(MenuData).map(([key,data],index)=>{
-        // let MenuItem =  Object.entries(MenuData).map((data,key,index)=>{
-            // console.log(data); 
-            // console.log(data[0]); 
-            // console.log(data[1]); 
-            // console.log("Kye is ", key," Value is ", data ); 
-            // console.log(key); 
-            // console.log(data); 
+
+        let MenuData = { "/": "Home", "/about": "About", "/examples": "Examples", "classcompo": "Class Compo" }
+
+        let MenuItem = Object.entries(MenuData).map(([key, data], index) => {
             return <li key={index}> <Link to={key}>{data}</Link> </li>
         })
         return (
             <div>
                 {/* test */}
+                <p>store your data in array as object </p>
+                <p>after that access that data </p>
                 {MenuItem}
                 {/* {printData}
                 {arr.forEach(element => {
