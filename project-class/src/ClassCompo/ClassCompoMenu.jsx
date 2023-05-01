6813b059094f21fab3ca4f2ba73ba4076c8d360e
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-05-01 11:31:36 +0530

    route

---
+++ project-class/src/ClassCompo/ClassCompoMenu.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+class ClassCompoIntro extends Component {
+  render() {
+    return (
+      <>
+        <div className="row accordion-item">
+          <div className="col-4 mx-auto">
+            <ul>
+              <li><Link to="classcompointro">Class Compo Intro</Link></li>
+              <li><Link to="classcompoconstructor">Constuctor</Link></li>
+              <li><Link to="classcompostate">State In Class Compo</Link></li>
+              <li><Link to="classcompojsx">JSX In Class Compo</Link></li>
+              <li><Link to="classcompoporps">Class Compo Props</Link></li>
+              <li><Link to="classcompostatelifecycle">Class Compo State LifeCycle</Link></li>
+              <li><Link to="classcompostatelifecycleloader">Class Compo State LifeCycle Loader</Link></li>
+              <li><Link to="classcompoconditionalrendering">Class Compo Conditional rendering</Link></li>
+              <li><Link to="classcompolistandkeys">Class Compo List and Keys</Link></li>
+            </ul>
+          </div>
+          <div className="col-8 ">
+            <Outlet></Outlet>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ClassCompoIntro;
--- project-class/src/ClassCompo/ClassCompoRoute.jsx
+++ project-class/src/ClassCompo/ClassCompoRoute.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ClassCompoMenu from './ClassCompoMenu';
+// import WelcomeToClassCompo from './01WelcomeToClassCompo.jsx';
+// import ClassCompoConstructor from './02ClassCompoConstructor.jsx';
+// import ClassCompoState from './03ClassCompoState.jsx';
+// import ClassCompojsx from './04ClassCompojsx.jsx';
+import ProspExample from './05Props';
+import ConditionalRendering from "./conditionalrender"
+// import StateLifeCycle from './07statelifecycle';
+// import ClassCompoLoader from './08statelifecycleloader';
+import ListAndKeys from "./07ListKeysArraymap"
 
 class ClassCompoRoute extends Component {
     render() {
         return (
             <>
-                
+                <Routes>
+                    <Route path="/" element={<ClassCompoMenu />} >
+                        {/* <Route path="classcompointro" element={<WelcomeToClassCompo />} /> */}
+                        {/* <Route path="classcompoconstructor" element={<ClassCompoConstructor />} /> */}
+                        {/* <Route path="classcompostate" element={<ClassCompoState />} /> */}
+                        {/* <Route path="classcompojsx" element={<ClassCompojsx />} /> */}
+                        <Route path="classcompoporps" element={<ProspExample />} />
+                        {/* <Route path="classcompostatelifecycle" element={<StateLifeCycle />} /> */}
+                        {/* <Route path="classcompostatelifecycleloader" element={<ClassCompoLoader />} /> */}
+                        <Route path="classcompoconditionalrendering" element={<ConditionalRendering />} />
+                        <Route path="classcompolistandkeys" element={<ListAndKeys/>} />
+                    </Route>
+                </Routes>
             </>
         );
     }
