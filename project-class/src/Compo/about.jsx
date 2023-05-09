2374280f6f7bf2f9e2035064f856255b579323f7
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-05-09 10:20:11 +0530

    data

--- project-class/src/Compo/about.jsx
+++ project-class/src/Compo/about.jsx
@@ -6,18 +6,10 @@ class About extends Component {
         return (
             <>
                 <div className="container">
-                    <h2>topics</h2>
-
-
-
-
+                    <h2 className='mt-3'>Components</h2>
                     <MDBAccordion className='' initialActive={1}>
-                        <MDBAccordionItem className='text-dark' collapseId={1} headerTitle='Statics'>
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse
-                            plugin adds the appropriate classes that we use to style each element. These classes control the overall
-                            appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with
-                            custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go
-                            within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        <MDBAccordionItem className='text-dark' collapseId={1} headerTitle='What is React Js ? '>
+                            React is a free and open-source front-end JavaScript library for building user interfaces based on components. It is maintained by Meta and a community of individual developers and companies. React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js
                         </MDBAccordionItem>
                         <MDBAccordionItem className='text-dark' collapseId={2} headerTitle='Accordion Item #2'>
                             <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse
