4ee0832b834b0218d08fe4ecef48f82027b72068
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-06-14 12:44:37 +0530

    data

--- for-delete/src/index.js
+++ for-delete/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import RouterProvider from "react-router-dom"
+// import RouterProvider from "react-router-dom"
+import { RouterProvider } from 'react-router-dom';
 import CustomStore from "./Store/CustomStore.jsx";
 import MainRouter from "./mainroutes"
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
