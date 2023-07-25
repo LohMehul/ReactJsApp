846f0c4e6415e5cbab8c4d027147dfb5ac43eae2
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-07-25 12:10:36 +0530

    data

--- project-class/src/Compo/funclifecycle.jsx
+++ project-class/src/Compo/funclifecycle.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-const funclifecycle = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+// const funclifecycle = () => {
+//     useEffect(() => {
+//         const intervalId = setInterval(() => {
+//         document.title = `Time is: ${new Date()}`;
+//         }, 1000);
+//         return () => {
+//           document.title = "Time stopped.";
+//           clearInterval(intervalId);
+//         }
+//       }, []);
+     
+//       return <h1>What time is it?</h1>;
+// };
+
+// export default funclifecycle;
+
+ 
+const Component = () => {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      document.title = `Time is: ${new Date()}`;
+    }, 1);
+ 
+    return () => {
+      document.title = "Time stopped.";
+      clearInterval(intervalId);
+    }
+  }, []);
+ 
+  return <h1>What time is it?</h1>;
 };
 
-export default funclifecycle;
+export default Component;
