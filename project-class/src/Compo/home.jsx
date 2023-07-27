728738373dcadc120238e5c4ad0c01d099b5363f
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-07-27 11:51:38 +0530

    data

--- project-class/src/Compo/home.jsx
+++ project-class/src/Compo/home.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 // import LifeCycleCompo from "./LifeCycleCompo.jsx";
-
-import {Link} from "react-router-dom"
+import "../style/home.css"
+import { Link } from "react-router-dom"
 const home = () => {
     return (
         <div className='container'>
-        <div className="row mt-3">
-            <div className="col mt-5 mb-5">
-            <h1>list of Assignment</h1>
-            <ul>
-                <li>
-                    <Link to ="./Examples"> examples</Link>
-                </li>
-            </ul>
+            <div className="row mt-3">
+                <div className="col mt-5 mb-5 title">
+                    <h1>list of Assignment</h1>
+                    <ul className='h-assignment'>
+                        <li>
+                            <Link className='link' to="/Examples"> examples</Link>
+                        </li>
+                        <li>
+                            <Link className='link' to="/components"> Components</Link>
+                        </li>
+                        <li>
+                            <Link className='link' to="/lifecycle"> LifeCycle</Link>
+                        </li>
+                        <li>
+                            <Link className='link' to="/Examples"> examples</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        {/* <LifeCycleCompo/>     */}
+            {/* <LifeCycleCompo/>*/}
         </div>
     );
 };
