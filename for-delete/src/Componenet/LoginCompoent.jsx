6351fd45a0e0a68ebc3467f769288476031ec893
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-09-20 13:03:32 +0530

    data

--- for-delete/src/Componenet/LoginCompoent.jsx
+++ for-delete/src/Componenet/LoginCompoent.jsx
@@ -84,26 +84,26 @@ const LoginCompoent = () => {
     event.preventDefault();
     console.log("registration data", inp);
     // fetch("http://localhost/API/registration", {
-    // fetch("http://localhost:5000/users", {
-    //   method: "POST", // *GET, POST, PUT, DELETE, etc.
-    //   // mode: "no-cors", // no-cors, *cors, same-origin
-    //   // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     // 'Content-Type': 'application/x-www-form-urlencoded',
-    //   },
-    //   body: JSON.stringify("sting",inp)
-    // }).then((res) => res.json()).then((result) => {
-    //   console.log("result",result);
-    //   navigate("/login");
-    // })
-    fetch('http://localhost:5000/users', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(inp)
-}).then(response =>{navigate("/login");console.log(response);})
+    fetch("http://localhost:5000/users", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      // mode: "no-cors", // no-cors, *cors, same-origin
+      // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      // headers: {
+      //   "Content-Type": "application/json",
+      //   // 'Content-Type': 'application/x-www-form-urlencoded',
+      // },
+      // body: JSON.stringify("sting",inp)
+    }).then((res) => res.json()).then((result) => {
+      console.log("result",result);
+      navigate("/login");
+    })
+//     fetch('http://localhost:5000/users', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(inp)
+// }).then(response =>{navigate("/login");console.log(response);})
    
 // output: 200
     // fetch(`https://jayramin.000webhostapp.com/loginget?username=${inp.username}&password=${inp.password}`).then((res) => res.json()).then((result) => {
